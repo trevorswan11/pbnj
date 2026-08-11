3619f6a85cc127dcea56f1dd6d9a619fba1dac01
trevorswan11 <tcs94@case.edu>
2026-08-10 23:00:51 -0400

    fix: Move mount text below component mounts

--- lib/ui/src/components/root.cc
+++ lib/ui/src/components/root.cc
@@ -9,10 +9,10 @@
 namespace pbnj::ui::components {
 
 auto root::on_mount(context& ctx) -> void {
-    ctx.log.info("Mounted root component");
     top_bar_.on_mount(ctx);
     side_bar_.on_mount(ctx);
     player_bar_.on_mount(ctx);
+    ctx.log.info("Mounted root component");
 }
 
 auto root::on_unmount(context& ctx) -> void {
